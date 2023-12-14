c16a1cad7a914eadc7ed2c8e415bba7b0881b34a
jtam2204 <jtam2204@gmail.com>
2023-12-14 23:46:43 +0000

    change car location

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const canvas=document.getElementById("myCanvas");
 canvas.width=200;
 
 const ctx = canvas.getContext("2d");
-const car=new Car(100,100,30,50);
+const car=new Car(100,700,30,50);
 
 animate();
 
